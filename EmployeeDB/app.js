43007cc583816022d17b5c35071e5ca4e4975360
schauhan <schauhan@LUS-SIDCHAUHAN1.teslamotors.com>
2015-10-17 12:36:35 -0700

    app.js bug fix

--- EmployeeDB/app.js
+++ EmployeeDB/app.js
@@ -13,7 +13,6 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.set('view options', {layout: false});
